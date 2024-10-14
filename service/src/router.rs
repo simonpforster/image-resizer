f99947b4d1769211059ecd61e6887433153a2c5f
Simon-Forster-6rf_nbcuni <simon.forster@sky.uk>
2024-10-14 13:14:52 +0100

    implement better error handling

--- service/src/router.rs
+++ service/src/router.rs
@@ -4,21 +4,23 @@ use http_body_util::{BodyExt, combinators::BoxBody, Full};
 use hyper::{Method, Request, Response, StatusCode};
 use hyper::body::Bytes;
 use log::info;
-use crate::service::process;
+use crate::service::{process, transform, InternalResponse, ResultResponse};
 
 pub async fn router(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>,  Box<dyn error::Error + Send + Sync>> {
     info!("Incoming {} request at {}", req.method().as_str(), req.uri());
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/private/status") => {
+    match (req.method(), req.uri().path(), req.uri().query()) {
+        (&Method::GET, "/private/status", None) => {
             let mut ok = Response::new(full("OK"));
             *ok.status_mut() = StatusCode::OK;
             Ok(ok)
         }
-        (&Method::GET, _) => {
-            // we'll be back
-            process(req).await
+        (&Method::GET, path, Some(query)) => {
+            transform(process(path))
+        }
+        (&Method::GET, path, None) => {
+            transform(process(path))
         }
         _ => {
             let mut not_found = Response::new(full("Endpoint not found"));
--- service/src/service.rs
+++ service/src/service.rs
@@ -6,74 +6,59 @@ use http_body_util::combinators::BoxBody;
 use hyper::{Request, Response, StatusCode};
 use hyper::body::{Frame, Bytes};
 use image::imageops::FilterType;
-use image::{ImageFormat, ImageReader};
+use image::{DynamicImage, ImageFormat, ImageReader};
 use log::{error, info, warn};
-use rand::Rng;
+use rand::{Rng};
+use crate::service::ErrorResponse::{ImageNotFoundError, ImageDecodeError, ImageWriteError};
+
+const PATH: &str = "/mnt/gcsfuse";
 
 const IMAGE_HEADER_NAME: &str = "content-type";
 const IMAGE_HEADER_ROOT: &str = "image";
 
-pub async fn process(req: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, Box<dyn error::Error + Send + Sync>> {
-    let path = String::from(req.uri().path());
-    //let parameters = req.uri().query();
+pub type ResultResponse = Result<Response<BoxBody<Bytes, hyper::Error>>, Box<dyn error::Error + Send + Sync>>;
+pub type InternalResponse = Result<Response<BoxBody<Bytes, hyper::Error>>, ErrorResponse>;
+
+
+pub enum ErrorResponse {
+    ImageNotFoundError { path: String },
+    ImageDecodeError { path: String },
+    ImageWriteError { path: String },
+}
+
+pub fn process(path: &str) -> InternalResponse {
     info!("Open image for path: {path}");
 
     let mut rng = rand::thread_rng();
 
-    let reader = match ImageReader::open(String::from("/mnt/gcsfuse") + &path) {
-        Ok(reader) => {
-            info!("Found image at {path}");
-            reader
-        }
-        Err(_) => {
-            error!("Could not find image at {path}");
-            return Ok(
-                Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(full("Image not found for: path"))
-                    .unwrap());
-        }
-    };
+    let reader = ImageReader::open(String::from(PATH) + &path).map_err(|_| {
+        error!("Could not find image at {path}");
+        ImageNotFoundError { path: path.to_string() }
+    })?;
+    info!("Found image at {path}");
 
-    let format = reader.format().unwrap_or_else(|| {
+    let format: ImageFormat = reader.format().unwrap_or_else(|| {
         warn!("Defaulting to Jpeg format for {path}");
         ImageFormat::Jpeg
     });
+    let format_extension: String = format.extensions_str().to_owned().iter().next().map(|ext| "/".to_owned() + ext).unwrap_or_else(|| "".to_owned());
+    info!("Image format found {format_extension}");
 
-    let image = match reader.decode() {
-        Ok(image) => {
-            info!("Image decoded at {path}");
-            image
-        }
-        Err(_) => {
-            error!("Could not decode image at {path}");
-            return Ok(
-                Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(full("Image could not be decoded for: path"))
-                    .unwrap());
-        }
-    };
+    let image = reader.decode().map_err(|_| {
+        error!("Could not decode image at {path}");
+        ImageDecodeError { path: path.to_string() }
+    })?;
+    info!("Image decoded at {path}");
 
-
-    let new_image = image.resize(rng.gen_range(20..image.width()), rng.gen_range(20..image.height()), FilterType::Nearest);
+    let new_image: DynamicImage = image.resize(rng.gen_range(20..image.width()), rng.gen_range(20..image.height()), FilterType::Nearest);
     info!("Image resized, writing image to buffer");
     let mut bytes: Vec<u8> = Vec::new();
     let mut cursor = Cursor::new(&mut bytes);
-    match new_image.write_to(&mut cursor, format) {
-        Ok(val) => {
-            info!("Image was written for {path}");
-            val
-        }
-        Err(_) => {
-            error!("Could not write the image for {path}");
-            return Ok(
-                Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(full("Image could not be written for: path"))
-                    .unwrap());
-        }
-    };
+    new_image.write_to(&mut cursor, format).map_err(|_| {
+        error!("Could not write the image for {path}");
+        ImageWriteError { path: path.to_string() }
+    })?;
+    info!("Image was written for {path}");
 
 
     let chunked = stream::iter(bytes).chunks(8192).map(|x| {
@@ -82,12 +67,11 @@ pub async fn process(req: Request<hyper::body::Incoming>) -> Result<Response<Box
     let body: BoxBody<Bytes, hyper::Error> = BoxBody::new(StreamBody::new(chunked));
     info!("Respond Success!");
 
-    let image_extension = format.extensions_str().to_owned().iter().next().map(|ext| "/".to_owned() + ext).unwrap_or_else(|| "".to_owned());
 
     let response =
         Response::builder()
             .status(StatusCode::OK)
-            .header(IMAGE_HEADER_NAME, IMAGE_HEADER_ROOT.to_owned() + &*image_extension)
+            .header(IMAGE_HEADER_NAME, IMAGE_HEADER_ROOT.to_owned() + &*format_extension)
             .body(body)
             .unwrap();
     Ok(response)
@@ -97,4 +81,42 @@ fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
     Full::new(chunk.into())
         .map_err(|never| match never {})
         .boxed()
+}
+
+pub fn transform(response: InternalResponse) -> ResultResponse {
+    match response {
+        Ok(resp) => Ok(resp),
+        Err(e) => match e {
+            ImageNotFoundError { path } => {
+                Ok(
+                    error_response(
+                        StatusCode::NOT_FOUND,
+                        format!("Image not found for: {path}"),
+                    )?
+                )
+            }
+            ImageDecodeError { path } => {
+                Ok(
+                    error_response(
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Image could not be decoded for: {path}"),
+                    )?
+                )
+            }
+            ImageWriteError { path } => {
+                Ok(
+                    error_response(
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Image could not be written for: {path}"),
+                    )?
+                )
+            }
+        }
+    }
+}
+
+pub fn error_response(status_code: StatusCode, message: String) -> hyper::http::Result<Response<BoxBody<Bytes, hyper::Error>>> {
+    Response::builder()
+        .status(status_code)
+        .body(full(message))
 }
