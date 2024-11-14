da31232bbce476607ac5282f7071fc3b366ae737
simonpforster <simon.forster@sky.uk>
2024-11-14 11:37:40 +0000

    culling log

--- service/src/cache.rs
+++ service/src/cache.rs
@@ -39,6 +39,7 @@ impl Cache {
                 path.to_string()
             }).collect();
         expired_paths.iter().for_each(|path| {
+            info!("Culling {}", path);
             self.map.remove(path);
         });
     }
