d168cd3b9d0ad09a12e63887628023935f4ab108
simonpforster <simon.forster@sky.uk>
2024-11-14 13:45:52 +0000

    clear fewer items

--- service/src/cache.rs
+++ service/src/cache.rs
@@ -20,34 +20,28 @@ impl Cache {
     }
 
     pub fn read_image(&self, path: &str) -> Option<&ImageCacheItem> {
-        let read_timer = Instant::now();
         let cache_item = self.map.get(path);
-        let elapsed = read_timer.elapsed().as_millis();
         match cache_item {
-            Some(_) => info!("Cache hit ({} ms): {}", elapsed, path),
-            None => info!("Cache miss ({} ms): {}", elapsed, path),
+            Some(_) => info!("Cache hit: {}", path),
+            None => info!("Cache miss: {}", path),
         }
         cache_item
     }
 
     pub fn write_image(&mut self, path: &str, cache_item: ImageCacheItem) -> () {
-        let read_timer = Instant::now();
         self.map.insert(path.to_string(), cache_item);
-        info!("Cache write ({} ms): {}", read_timer.elapsed().as_millis(), path);
+        info!("Cache write: {}", path);
     }
 
     pub fn cull(&mut self) -> () {
-        let find_expired_timer = Instant::now();
+        let cull_timer = Instant::now();
         let expired_paths: Vec<String> = self.map.iter()
             .filter(|(_, item)| {
                 item.time.elapsed() >= Duration::from_secs(300)
             })
             .map(|(path, _)| {
                 path.to_string()
-            }).collect();
-        let expired_elapsed = find_expired_timer.elapsed().as_millis();
-        info!("Expired found ({} ms).", expired_elapsed);
-        let cull_timer = Instant::now();
+            }).take(5).collect();
         expired_paths.iter().for_each(|path| { self.map.remove(path); });
         info!("Cache cleared ({} ms) {} items.",  cull_timer.elapsed().as_millis(), expired_paths.len());
     }
