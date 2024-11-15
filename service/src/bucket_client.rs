64fc631f334b3fd346a4b3840efc645f8fecaa6a
simonpforster <simon.forster@sky.uk>
2024-11-15 12:26:46 +0000

    final

--- service/src/bucket_client.rs
+++ service/src/bucket_client.rs
@@ -1,7 +1,5 @@
 use lazy_static::lazy_static;
 use log::info;
-use reqwest::Version;
-use tokio::time::Instant;
 
 const BUCKET_URL: &str = "https://storage.googleapis.com/image-resizer_europe-west1";
 
