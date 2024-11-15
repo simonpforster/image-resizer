65e7f448318f83e696eac3a0b47e88358a8892e7
simonpforster <simon.forster@sky.uk>
2024-11-15 09:08:19 +0000

    logging

--- service/src/bucket_client.rs
+++ service/src/bucket_client.rs
@@ -1,11 +1,16 @@
 use image::EncodableLayout;
+use log::info;
+use tokio::time::Instant;
 
 const BUCKET_URL: &str = "https://storage.googleapis.com/image-resizer_europe-west1";
 
 
 pub async fn bucket_request(path: &str) -> Result<Vec<u8>, reqwest::Error> {
+    let http_request_timer = Instant::now();
     let url = String::from(BUCKET_URL) + path;
     let resp = reqwest::get(&url).await?;
     let bytes = resp.bytes().await.map(|d| {d.as_bytes().to_vec()});
+    info!("Code {} after {} for {}", resp.status(), http_request_timer.elapsed().as_millis(), path);
+    info!("headers", resp.headers());
     bytes
 }
