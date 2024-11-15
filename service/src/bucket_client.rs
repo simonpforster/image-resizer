ea10d23e16b849eb82ff1e235e6717e9889fe51b
simonpforster <simon.forster@sky.uk>
2024-11-15 12:13:25 +0000

    reduce impl

--- service/src/bucket_client.rs
+++ service/src/bucket_client.rs
@@ -11,20 +11,9 @@ lazy_static! {
 }
 
 pub async fn bucket_request(path: &str) -> Result<Vec<u8>, reqwest::Error> {
-    let http_request_timer = Instant::now();
     let url = String::from(BUCKET_URL) + path;
-    info!("sending request");
-    let fut = CLIENT.get(&url).send();
-    info!("sent request");
-    let resp = fut.await?;
-    info!("got response");
-    let status = resp.status();
-    info!("Code: {}", status);
-    let headers = resp.headers().clone();
-    let version = resp.version();
+    let resp = CLIENT.get(&url).send().await?;
     let bytes = resp.bytes().await.map(|d| { d.to_vec() });
-    info!("Code {}:{:?} after {} for {}", status, version, http_request_timer.elapsed().as_millis(), path);
-    info!("headers {:#?}", headers);
     bytes
 }
 
