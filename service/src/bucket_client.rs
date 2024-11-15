c1b5b8ef5fb59b70c91591a0ad66e3ad9cc9d50d
simonpforster <simon.forster@sky.uk>
2024-11-15 09:21:56 +0000

    ???

--- service/src/bucket_client.rs
+++ service/src/bucket_client.rs
@@ -1,16 +1,21 @@
-use image::EncodableLayout;
+use lazy_static::lazy_static;
 use log::info;
 use tokio::time::Instant;
 
 const BUCKET_URL: &str = "https://storage.googleapis.com/image-resizer_europe-west1";
 
+lazy_static! {
+    static ref CLIENT: reqwest::Client = reqwest::Client::new();
+}
 
 pub async fn bucket_request(path: &str) -> Result<Vec<u8>, reqwest::Error> {
     let http_request_timer = Instant::now();
     let url = String::from(BUCKET_URL) + path;
-    let resp = reqwest::get(&url).await?;
-    let bytes = resp.bytes().await.map(|d| {d.as_bytes().to_vec()});
-    info!("Code {} after {} for {}", resp.status(), http_request_timer.elapsed().as_millis(), path);
-    info!("headers {}", resp.headers());
+    let resp = CLIENT.get(&url).send().await?;
+    let status = resp.status();
+    let headers  = resp.headers().clone();
+    let bytes = resp.bytes().await.map(|d| { d.to_vec() });
+    info!("Code {} after {} for {}", status, http_request_timer.elapsed().as_millis(), path);
+    info!("headers {:?}", headers);
     bytes
 }
