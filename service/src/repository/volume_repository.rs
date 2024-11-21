1ceaf11291b65fdc2e294d629433484808d499fe
simonpforster <simon.forster@sky.uk>
2024-11-21 11:13:26 +0000

    more

--- service/src/repository/volume_repository.rs
+++ service/src/repository/volume_repository.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::time::Instant;
 use image::ImageFormat;
-use log::{error, warn};
+use log::{error, info, warn};
 use crate::repository::{ImageItem, ImageRepository};
 use crate::service::{ErrorResponse, ImageNotFoundError, ImageWriteError};
 
@@ -11,18 +11,21 @@ const ROOT_PATH: &str = "/mnt/shared-cache/";
 
 impl VolumeRepository {
     pub async fn write_image(&self, path: &str, cache_item: &ImageItem) -> Result<(), ErrorResponse> {
+        let timer = Instant::now();
         fs::write(ROOT_PATH.to_string() + path, &cache_item.image).map_err(|_| {
             error!("Could not write image at {path}");
             ImageWriteError {
                 path: path.to_string(),
             }
         })?;
+        info!("FS write took {} ms for {}", timer.elapsed().as_millis(), path);
         Ok(())
     }
 }
 
 impl ImageRepository for VolumeRepository {
     async fn read_image(&self, path: &str) -> Result<ImageItem, ErrorResponse> {
+        let timer = Instant::now();
         let format: ImageFormat = ImageFormat::from_path(path).unwrap_or_else(|_| {
             warn!("Defaulting to Jpeg format for {path}");
             ImageFormat::Jpeg
@@ -34,7 +37,7 @@ impl ImageRepository for VolumeRepository {
                 path: path.to_string(),
             }
         })?;
-
+        info!("FS read took {} ms for {}", timer.elapsed().as_millis(), path);
         Ok(ImageItem {
             time: Instant::now(),
             format,
